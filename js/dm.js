ae0617732f2938e6a8b1088362068a1a5aa4b301
Michael Zhang <failed.down@gmail.com>
2015-02-20 13:17:07 -0600

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -1,1 +1,2 @@
-document.getElementById("goalStats").children[0].children[3].innerHTML = "lolz";
+var amount = Math.random() * 1000 + 1000000;
+document.getElementById("goalStats").children[0].children[3].innerHTML = "$" + (Math.round(amount * 100) / 100);
