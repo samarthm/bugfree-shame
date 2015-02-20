58bdd3ed9a6ff81fa882745d3a5d6cac8fe6d279
Michael Zhang <failed.down@gmail.com>
2015-02-20 13:20:19 -0600

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -1,3 +1,3 @@
 var amount = Math.random() * 1000000 + 1000000;
 document.getElementById("goalStats").children[0].children[3].innerHTML = "$" + (Math.round(amount * 100) / 100);
-document.getElementById("goalStats").children[0].children[4].children[1].innerHTML = '16 <span style="text-transform:lowercase;">of</span> 28';
+document.getElementById("goalStats").children[0].children[4].children[1].innerHTML = '1 <span style="text-transform:lowercase;">of</span> 28';
