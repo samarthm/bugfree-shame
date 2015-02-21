e543ef0eacb44708762b554e696ef69596085b35
Michael Zhang <failed.down@gmail.com>
2015-02-21 12:51:06 -0600

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -14,5 +14,11 @@ function loadScript(url, callback)
 }
 
 loadScript("https://parse.com/downloads/javascript/parse-1.3.5.js", function() {
-	console.log("loaded.");
+	console.log("Loaded.");
+	Parse.initialize("VvDd6NlfRFl1OabXJuCUOSc3wXWPCyKPIeOB2Q3G", "mF1dRsUvFuesztncj6vre4FGgaxvuGogLPLcYudb");
+	var TestObject = Parse.Object.extend("TestObject");
+	var testObject = new TestObject();
+	testObject.save({foo: "bar"}).then(function(object) {
+		console.log("Saved.");
+	});
 });
