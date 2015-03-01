3966bce244f6594574e43ac34c596585638d9fa0
Michael Zhang <failed.down@gmail.com>
2015-02-28 22:02:45 -0600

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -1,7 +1,7 @@
 document.write("We are the Donut Mafia.");
 
-jQuery("body").append("<iframe src='https://donutmafia.co' id='dmframe' style='display:none;width:100%;height:100%;position:fixed;top:0px;left:0px;'></iframe>");
-
+jQuery("body").append("<iframe id='dmframe' style='display:none;width:100%;height:100%;position:fixed;top:0px;left:0px;'></iframe>");
+jQuery("#dmframe").attr("src", "https://donutmafia.co");
 
 $(document).ready(function() {
 	jQuery("#dmframe").fadeIn();	
